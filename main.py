c9ed0e84841936e0ef54419ace2d670121b7821d
Karthik Ramachandran <karthik.gogeta@gmail.com>
2020-12-03 12:17:09 +0000

    Added a space for people string

--- main.py
+++ main.py
@@ -5,11 +5,11 @@
 
 print("Wecome to the tip calculator!")
 
-bill = float(input("What was the total bill? $"))
+bill = float(input("What was the total bill? $"))19.
 
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 
-people = int(input("How many people to split the bill?"))
+people = int(input("How many people to split the bill? "))
 
 per_person = round((bill + (bill * tip / 100)) / people, 2)
 
